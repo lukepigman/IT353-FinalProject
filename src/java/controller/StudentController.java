393c9a24cccd7b64a5c047be7910a39cf35b2d93
lukepigman <ldpigma@ilstu.edu>
2018-12-02 15:15:53 -0600

    upadates

--- src/java/controller/StudentController.java
+++ src/java/controller/StudentController.java
@@ -7,8 +7,16 @@ package controller;
 
 import DAO.StudentDAOImpl;
 import java.util.ArrayList;
+import java.util.Properties;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import model.StudentBean;
 
 /**
@@ -168,6 +176,57 @@ public class StudentController {
         }
     }   
 
+    public void sendEmail() {
+
+        // Recipient's email ID needs to be mentioned.
+        String to = theModel.getEmail();
+
+        // Sender's email ID needs to be mentioned
+        String from = "xxx@ilstu.edu";
+        
+        // Assuming you are sending email from this host
+        String host = "outlook.office365.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.port", "587");
+        // Get the default Session object.
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("ldpigma@istu.edu", "your password");
+            }
+        });
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("Congratulations!");
+
+            // Send the actual HTML message, as big as you like
+            message.setContent("<h1>You have been promoted to the VP of Student Affairs!</h1>",
+                    "text/html");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
     /**
      * @return the searchTest
      */
